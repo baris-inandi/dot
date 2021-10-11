206aa5941e2164539e640923a5078e361f379691
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-10-11 19:47:51 +0300

    progress

--- install.sh
+++ install.sh
@@ -10,10 +10,19 @@ paru -S --noreview --noconfirm < ./pkg/postinstall
 chsh --shell /usr/bin/zsh
 
 # symlinks
-ln -f -s ~/dot/i3config ~/.config/i3/config
+ln -f -s ~/dot/i3 ~/.config/i3/config
 ln -f -s ~/dot/kitty.conf ~/.config/kitty/kitty.conf
 ln -f -s ~/dot/.gitconfig ~/.gitconfig
 ln -f -s ~/dot/.zshrc ~/.zshrc
 
+# configure keyboard layout
+sudo cp -f ~/dot/vconsole.conf /etc/vconsole.conf
+
+# default wallpaper
+cp -f ~/dot/wallpaper.png ~/Pictures/wallpaper.png
+
 # update everything
 paru
+
+# restart
+sudo reboot
