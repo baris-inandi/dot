79edd3e937a43b02e03a7e8cde12a81af1ca90b2
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2021-10-11 18:29:50 +0300

    progress

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo pacman -S paru
+# install paru helper
+
+paru -S --noreview --noconfirm < postinstall
+# download and install all postinstall packages
+
+# chsh zsh here
+# chsh where zsh
+
+# symlinks here
+
+paru
+# update everything
