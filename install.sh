065fb3c4f9531a41067bc6ccebda5d729b556157
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2021-10-12 08:44:04 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 # install paru helper
 sudo pacman -Sy
+wait
 sudo pacman -S paru
+wait
 
 # download and install all fonts + postinstall packages
 paru -S --noreview --noconfirm < ./pkg/font
+wait
 paru -S --noreview --noconfirm < ./pkg/postinstall
+wait
 
 # zsh as deafult shell
 chsh --shell /usr/bin/zsh
+wait
 
 # symlinks
 ln -f -s ~/dot/i3 ~/.config/i3/config
@@ -15,14 +20,19 @@ ln -f -s ~/dot/kitty.conf ~/.config/kitty/kitty.conf
 ln -f -s ~/dot/.gitconfig ~/.gitconfig
 ln -f -s ~/dot/.zshrc ~/.zshrc
 
+wait
+
 # configure keyboard layout
 sudo cp -f ~/dot/vconsole.conf /etc/vconsole.conf
+wait
 
 # default wallpaper
 cp -f ~/dot/wallpaper.png ~/Pictures/wallpaper.png
+wait
 
 # update everything
 paru
+wait
 
 # restart
 sudo reboot
