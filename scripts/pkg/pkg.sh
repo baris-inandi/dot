81e7ff66bf6fe76b2b55f3a0f35004f96bab9999
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-11-21 21:37:34 +0300

    install omf

--- scripts/pkg/pkg.sh
+++ scripts/pkg/pkg.sh
@@ -1,3 +1,10 @@
-# fonts and essential packages
+# essential packages
+
+# install basic fonts
 paru -S --skipreview --noconfirm ttf-ms-fonts ttf-apple-emoji ttf-fira-sans ttf-fira-mono ttf-fira-code ttf-droid ttf-dejavu inter-font noto-fonts adobe-source-code-pro-fonts awesome-terminal-fonts ttf-cascadia-code ttf-font-awesome ttf-hack ttf-inconsolata ttf-jetbrains-mono ttf-lato ttf-liberation powerline-fonts terminus-font cantarell-fonts
-paru -S --skipreview --noconfirm fish picom maim nano micro python310 python-pip npm pfetch sudo unrar unzip vim wget xautolock google-chrome spotify visual-studio-code-bin go ranger trash-cli hollywood speedtest-cli kitty github-cli i3-gaps xclip maim cat bat cowsay feh libnotify notify-send dunst brightnessctl playerctl neofetch
+
+# install some other packages
+paru -S --skipreview --noconfirm fish picom-jonaburg-git betterlockscreen maim nano micro python310 python-pip npm pfetch sudo unrar unzip vim wget xautolock google-chrome spotify visual-studio-code-bin go ranger trash-cli hollywood speedtest-cli kitty github-cli i3-gaps xclip maim cat bat cowsay feh libnotify notify-send dunst brightnessctl playerctl neofetch gnome-keyring libsecret libgnome-keyring seahorse
+
+# install oh my fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
