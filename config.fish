7b819175a91bcae0173fb09b4017bd93556daf29
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-12-14 10:23:14 +0300

    monkeytype

--- config.fish
+++ config.fish
@@ -2,6 +2,7 @@ function gsync -a name ; sh ~/dot/scripts/user/gitsync.sh $name ; end
 function clone -a name ; sh ~/dot/scripts/user/gitclone.sh $name ; end
 function vs ; code . && exit ; end
 function github -a repo ; firefox https://github.com/baris-inandi/$repo ; end
+function monk ; firefox --new-window https://monkeytype.com ; exit ; end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
