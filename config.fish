13bfcdfe27e23db78d9e9eacbde9592725d5ca95
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-12-14 10:19:03 +0300

    add github script

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 function gsync -a name ; sh ~/dot/scripts/user/gitsync.sh $name ; end
 function clone -a name ; sh ~/dot/scripts/user/gitclone.sh $name ; end
 function vs ; code . && exit ; end
+function github -a repo ; firefox https://github.com/baris-inandi/$repo ; end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
