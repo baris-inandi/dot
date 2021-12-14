e85a52ca3fd0104efd4e290121445db8507f57f1
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-12-14 21:55:18 +0300

    fix

--- scripts/user/dotautopull.sh
+++ scripts/user/dotautopull.sh
@@ -7,7 +7,7 @@ if [[ $(git diff) = "" ]]; then
   notify-send "Updating dot" "Your dotfiles are unchanged locally, syncing it from the remote..."
   git pull
   new_hash=`git rev-parse --short --verify HEAD`
-  if [[ "$local_hash" == "$new_hash" ]]; then
+  if [[ "$local_hash" != "$new_hash" ]]; then
   	notify-send "Updated dot" "dot is currently at $new_hash"
   fi
 fi
