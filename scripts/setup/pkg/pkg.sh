db103a1249c69ae78546fb18870903a2f7f04631
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-12-21 10:30:25 +0300

    prog

--- scripts/setup/pkg/pkg.sh
+++ scripts/setup/pkg/pkg.sh
@@ -8,3 +8,6 @@ source ~/dot/scripts/setup/pkg/pkglist/basic.sh
 
 # install oh my fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+# install desired python modules
+pip3 install dbus-python py3status
