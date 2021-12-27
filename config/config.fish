fc8a0f7289dcb90f94cbc730c00730ed765c60d0
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-12-27 15:26:50 +0300

    typingtest

--- config/config.fish
+++ config/config.fish
@@ -1,6 +1,7 @@
 function gsync -a name ; bash ~/dot/scripts/user/fish/gitsync.sh $name ; end
 function clone -a name ; bash ~/dot/scripts/user/fish/gitclone.sh $name ; end
 function vs -a vsdir ; bash ~/dot/scripts/user/fish/vs.sh $vsdir ; exit ; end
+function ttt ; tt -blockcursor -notheme -showwpm -bold -words 200en -highlight1 ; end
 
 # web shortcuts
 function classroom ; firefox "https://classroom.google.com/u/1/h" ; exit ; end
