fadbc1fd63b79a7017ca62bdb0082176a6868f4f
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-12-27 15:33:47 +0300

    prog

--- scripts/user/fish/gitsync.sh
+++ scripts/user/fish/gitsync.sh
@@ -5,18 +5,25 @@ normal=$(tput sgr0)
 green=$(tput setaf 2)
 white=$(tput sgr0)
 
-printf "\n"
+if [ -z "$1" ]; then
+  echo
+  echo "GITSYNC"
+  echo "please specify a commit message."
+  echo "usage: gitsync <COMMIT_MESSAGE>"
+  echo
+else
+  printf "${bold}${green}GITSYNC:${white} Pulling from remote...\n${normal}"
+  git pull
 
-printf "${bold}${green}GITSYNC:${white} Pulling from remote...\n\n${normal}"
-git pull
+  printf "${bold}${green}GITSYNC:${white} Adding all changes...\n${normal}"
+  git add -A
 
-printf "${bold}${green}GITSYNC:${white} Adding all changes...\n\n${normal}"
-git add -A
+  printf "${bold}${green}GITSYNC:${white} Committing...\n${normal}"
+  git commit -am "$1"
 
-printf "${bold}${green}GITSYNC:${white} Committing...\n\n${normal}"
-git commit -am "$1"
+  printf "${bold}${green}GITSYNC:${white} Pushing...\n${normal}"
+  git push
 
-printf "${bold}${green}GITSYNC:${white} Pushing...\n\n${normal}"
-git push
+  printf "${bold}${green}REPO SYNCED${white}\n${normal}"
 
-printf "${bold}${green}REPO SYNCED${white}\n${normal}"
+fi
