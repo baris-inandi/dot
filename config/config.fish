b3ffc9cc6cf03eaa8c2e336819e287cc18a5d47b
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-01-02 13:44:53 +0300

    progress

--- config/config.fish
+++ config/config.fish
@@ -3,6 +3,7 @@ function clone -a name ; bash ~/dot/scripts/user/fish/gitclone.sh $name ; end
 function vs -a vsdir ; bash ~/dot/scripts/user/fish/vs.sh $vsdir ; exit ; end
 function ttt ; tt -blockcursor -notheme -showwpm -bold -words 200en -highlight1 ; end
 function c ; clear ; end
+function mk -a dir ; mkdir $dir ; cd $dir ; end
 
 # web shortcuts
 function classroom ; firefox "https://classroom.google.com/u/1/h" ; exit ; end
