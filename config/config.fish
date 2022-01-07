6afa78810b0e3836dbd93a2a19dc9616a82335ae
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-01-07 14:22:20 +0300

    progress

--- config/config.fish
+++ config/config.fish
@@ -1,9 +1,10 @@
 function gsync -a name ; bash ~/dot/scripts/user/fish/gitsync.sh $name ; end
-function clone -a name ; bash ~/dot/scripts/user/fish/gitclone.sh $name ; end
+function clone -a name ; bash ~/dot/scripts/user/fish/gitclone.sh $name ; wait ; cd $name ; end
 function vs -a vsdir ; bash ~/dot/scripts/user/fish/vs.sh $vsdir ; exit ; end
 function ttt ; tt -blockcursor -notheme -showwpm -bold -words 200en -highlight1 ; end
-function c ; clear ; end
 function mk -a dir ; mkdir $dir ; cd $dir ; end
+alias vi="~/.local/bin/lvim"
+alias c="clear"
 
 # web shortcuts
 function classroom ; firefox "https://classroom.google.com/u/1/h" ; exit ; end
