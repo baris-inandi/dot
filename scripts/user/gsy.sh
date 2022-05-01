3fbbc09124eeb9d6eda0bcef81683b74c5cb40cf
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-05-01 15:13:42 +0300

    commit

--- scripts/user/gsy.sh
+++ scripts/user/gsy.sh
@@ -1,20 +1,16 @@
-async() {
-    if [ -d .git ]; then
-        if [ -z "$1" ]; then
-            cm=commit
-        else
-            cm="$1"
-        fi
-
-        notify-send "GSY" "Syncing $PWD"
-        notify-send "git add -A" "$(git add -A)"
-        notify-send "git commit -am $cm" "$(git commit -am $cm)"
-        git push
-        notify-send "Synced $PWD" "Repo currently at $(git rev-parse --short HEAD)"
-
+if [ -d .git ]; then
+    if [ -z "$1" ]; then
+        cm=commit
     else
-        notify-send "GSY FATAL" "Not a git repo"
+        cm="$1"
     fi
-}
-# test
-nohup "$(async)" >/dev/null 2>&1
+
+    notify-send "GSY" "Syncing $PWD"
+    notify-send "git add -A" "$(git add -A)"
+    notify-send "git commit -am $cm" "$(git commit -am $cm)"
+    git push
+    notify-send "Synced $PWD" "Repo currently at $(git rev-parse --short HEAD)"
+
+else
+    notify-send "GSY FATAL" "Not a git repo"
+fi
