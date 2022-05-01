ad3b14769155707e2540cb4175956140cedef334
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-05-01 15:21:42 +0300

    commit

--- scripts/user/gsy.sh
+++ scripts/user/gsy.sh
@@ -7,7 +7,7 @@ if [ -d .git ]; then
     notify-send -t 50000 "GSY" "Syncing $PWD"
     notify-send -t 50000 "git add -A" "$(git add -A)"
     notify-send -t 50000 "git commit -am $cm" "$(git commit -am $cm)"
-    notify-send -t 50000 "Syncing $PWD" "$(git push &)" &
+    notify-send -t 50000 "Syncing $PWD" "$(git push)" &
     notify-send -t 50000 "Synced $PWD" "Repo currently at $(git rev-parse --short HEAD)" >/dev/null 2>&1 &
 else
     notify-send "GSY FATAL" "Not a git repo"
