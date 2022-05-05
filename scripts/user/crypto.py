f7a22eabed0938081d6de6195db65a7ddac0bf95
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-05-05 20:07:20 +0300

    commit

--- scripts/user/crypto.py
+++ scripts/user/crypto.py
@@ -1,4 +1,5 @@
 from os import system, path, remove
+from sys import argv
 import grequests
 import time
 import json
@@ -48,76 +49,80 @@ def fetch() -> dict[str, dict[str, str]]:
     return out
 
 
-def main():
+def get_cmds() -> dict[str, str]:
     try:
         if path.isfile(LOCKFILE):
             return
         open(LOCKFILE, "w")
-        cumulative_time = 0
         if is_cached():
             with open(CACHE_FILE, "r") as f:
-                out = json.load(f)["cache"]
+                return json.load(f)["cache"]
         else:
             out = fetch()
+            cmds = {}
+            for k, v in out.items():
+                k = k.upper()
+
+                price = float(v["buy"])
+                change_rate = float(v["changeRate"])
+                change_rate_str = trailing_round(change_rate, 2)
+                change_price = float(v["changePrice"])
+                avg_price = float(v["averagePrice"])
+                above_avg = price > avg_price
+
+                if price > 1000:
+                    price = round(float(v["buy"]))
+                    change_price_str = str(round(change_price))
+                    avg_price_str = str(round(avg_price))
+                else:
+                    precision = 3
+                    if price > 100:
+                        precision = 1
+                    elif price > 10:
+                        precision = 2
+                    price = trailing_round(float(v["buy"]), precision)
+                    change_price_str = trailing_round(change_price, precision)
+                    avg_price_str = trailing_round(avg_price, precision)
+
+                if change_price < 0:
+                    change_price_str = f"\\{change_price_str}"
+
+                up_down = (
+                    "Short " + DOWN_SYMBOL if change_price < 0 else "Long " + UP_SYMBOL
+                ) + " "
+                msg_header = f"{k} | {price} USD"
+                msg_header = pad_str(MSG_WIDTH, msg_header, up_down)
+                msg_fmt = pad_str(
+                    MSG_WIDTH, f"{change_price_str} USD", f"{change_rate_str}%"
+                )
+                msg_fmt += "\n" + pad_str(
+                    MSG_WIDTH,
+                    f"{avg_price_str} USD avg.",
+                    "Above avg." if above_avg else "Below avg.",
+                )
+                cmd = f'notify-send "{msg_header}" "{msg_fmt}" -t {TIME}'
+                cmds[k] = cmd
             with open(CACHE_FILE, "w") as f:
                 json.dump(
                     {
                         "timestamp": round(time.time()),
                         "conf": str(CONF["tickers"]),
-                        "cache": out,
+                        "cache": cmds,
                     },
                     f,
                 )
-        for k, v in out.items():
-            start = time.time()
-            k = k.upper()
-
-            price = float(v["buy"])
-            change_rate = float(v["changeRate"])
-            change_rate_str = trailing_round(change_rate, 2)
-            change_price = float(v["changePrice"])
-            avg_price = float(v["averagePrice"])
-            above_avg = price > avg_price
-
-            if price > 1000:
-                price = round(float(v["buy"]))
-                change_price_str = str(round(change_price))
-                avg_price_str = str(round(avg_price))
-            else:
-                precision = 3
-                if price > 100:
-                    precision = 1
-                elif price > 10:
-                    precision = 2
-                price = trailing_round(float(v["buy"]), precision)
-                change_price_str = trailing_round(change_price, precision)
-                avg_price_str = trailing_round(avg_price, precision)
-
-            if change_price < 0:
-                change_price_str = f"\\{change_price_str}"
-
-            up_down = (
-                "Short " + DOWN_SYMBOL if change_price < 0 else "Long " + UP_SYMBOL
-            ) + " "
-            msg_header = f"{k} | {price} USD"
-            msg_header = pad_str(MSG_WIDTH, msg_header, up_down)
-            msg_fmt = pad_str(
-                MSG_WIDTH, f"{change_price_str} USD", f"{change_rate_str}%"
-            )
-            msg_fmt += "\n" + pad_str(
-                MSG_WIDTH,
-                f"{avg_price_str} USD avg.",
-                "Above avg." if above_avg else "Below avg.",
-            )
-            system(
-                f'notify-send "{msg_header}" "{msg_fmt}" -t {str(TIME - cumulative_time)}'
-            )
-            end = time.time()
-            elapsed = round((end - start) * 1000) + 7
-            cumulative_time += elapsed
+            return cmds
     finally:
         remove(LOCKFILE)
 
 
+def main():
+    cmds = get_cmds()
+    for cmd in cmds.values():
+        system(cmd)
+
+
 if __name__ == "__main__":
+    if len(argv) > 1 and argv[1] == "reset":
+        remove(CACHE_FILE)
     main()
