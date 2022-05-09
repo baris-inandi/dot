42c21fd8262bb97dfc1df909d8e3601ea9372bfc
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-05-09 11:28:43 +0300

    commit

--- scripts/user/gsync.sh
+++ scripts/user/gsync.sh
@@ -5,24 +5,20 @@ normal=$(tput sgr0)
 color=$(tput setaf 5)
 white=$(tput sgr0)
 
-if [ -d .git ]; then
-  if [ -z "$1" ]; then
-    cm=commit
-  else
-    cm="$1"
-  fi
-
-  printf "${bold}${color}GSYNC:${white} Syncing \"$cm\"\n${normal}"
-  printf "${bold}${color}GSYNC:${white} Adding all changes...\n${normal}"
-  git add -A
-
-  printf "${bold}${color}GSYNC:${white} Committing...\n${normal}"
-  git commit -am "$cm"
-
-  printf "${bold}${color}GSYNC:${white} Pushing...\n${normal}"
-  git push
-
-  printf "${bold}${color}REPO SYNCED${white}\n${normal}"
+if [ -z "$1" ]; then
+  cm=commit
 else
-  printf "${bold}${color}GSYNC:${white} Not a git repo\n${normal}"
+  cm="$1"
 fi
+
+printf "${bold}${color}GSYNC:${white} Syncing \"$cm\"\n${normal}"
+printf "${bold}${color}GSYNC:${white} Adding all changes...\n${normal}"
+git add -A
+
+printf "${bold}${color}GSYNC:${white} Committing...\n${normal}"
+git commit -am "$cm"
+
+printf "${bold}${color}GSYNC:${white} Pushing...\n${normal}"
+git push
+
+printf "${bold}${color}REPO SYNCED${white}\n${normal}"
