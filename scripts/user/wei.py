dddb95655dcb05ff05df99dace3899bcfa5b9f4b
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-05-09 23:33:25 +0300

    commit

--- scripts/user/wei.py
+++ scripts/user/wei.py
@@ -10,14 +10,17 @@ def main():
     try:
         if len(argv) > 1:
             num = argv[1]
+            if num == "cp":
+                clipboard.copy("1000000000000000000")
+                print("Copied one wei to clipboard.")
+                exit(0)
             print(int(num) / 10**18)
         else:
             num = clipboard.paste()
             print(int(num) / 10**18)
     except Exception as e:
         print(f'Wei: Cannot parse input:\n"{num}"')
-    finally:
-        print("[1 ETH = 1000000000000000000 wei]")
+    print("[1 ETH = 1000000000000000000 wei]")
 
 
 if __name__ == "__main__":
