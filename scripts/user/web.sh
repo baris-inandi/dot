8fa6af86ef364b5349b40f6df5b965b5252706db
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-05-21 16:53:09 +0300

    commit

--- scripts/user/web.sh
+++ scripts/user/web.sh
@@ -1,3 +1,3 @@
-google-chrome-unstable -new-tab "$2"
+i3-msg "mode default" &
+google-chrome-unstable -new-tab "$2" &
 notify-send "$1" "Launching web shortcut in browser..." -t 1000
-i3-msg "mode default"
