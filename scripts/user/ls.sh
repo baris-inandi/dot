0d254c6ba86469349623f44becc0fcfa8a6cb648
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-06-08 00:34:09 +0300

    commit

--- scripts/user/ls.sh
+++ scripts/user/ls.sh
@@ -1,5 +1,5 @@
 call_exa() {
-  exa --group-directories-first -laFh --git --no-user --no-time --octal-permissions --no-permissions
+  exa --group-directories-first -laFh --git --no-user --no-time --octal-permissions --no-permissions $@
 }
 
-call_exa || ls
+call_exa $@ || ls
