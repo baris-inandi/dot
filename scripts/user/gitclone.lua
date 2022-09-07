22afd26e674d5ba2f036631c9ea63b237c93cc51
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2022-09-07 18:31:24 +0300

    Update gitclone.lua

--- scripts/user/gitclone.lua
+++ scripts/user/gitclone.lua
@@ -1,11 +1,27 @@
-if arg[1] then
-  if string.find(arg[1], "/") then
-    Repo = arg[1]
-  else
-    Repo = "baris-inandi/" .. arg[1]
-  end
-  local cmd = "git clone https://github.com/" .. Repo
-  os.execute(cmd)
-else
-  print("Usage: gitclone <GitHub Repo>")
+local cmd_base = "git clone "
+local acc_uname = "baris-inandi"
+local cmd
+
+if not arg[1] or string.sub(arg[1], 1, 1) == "-" then
+  print("Usage:")
+  print("  gitclone <url>")
+  print("  gitclone <repo>")
+  print("  gitclone <user/repo>")
+  os.exit(0)
 end
+if string.find(arg[1], "://") then
+  -- Is a URL
+  -- directly clone it
+  cmd = cmd_base .. arg[1]
+elseif string.find(arg[1], "/") then
+  -- is a user/repo
+  -- clone it from github
+  cmd = cmd_base .. "https://github.com/" .. arg[1]
+else
+  -- is just a repo name
+  -- clone from personal account
+  cmd = cmd_base .. "https://github.com/" .. acc_uname .. "/" .. arg[1]
+end
+
+print(cmd)
+os.execute(cmd)
