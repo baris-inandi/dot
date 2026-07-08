d30849298a004375f406fe47b668c2b74cefc545
Baris Inandioglu <68742481+baris-inandi@users.noreply.github.com>
2026-07-08 16:40:35 -0400

    starship config

--- setup/base/symlink.sh
+++ setup/base/symlink.sh
@@ -14,4 +14,5 @@ symlink ~/dot/config/micro/micro.json ~/.config/micro/settings.json
 symlink ~/dot/config/micro/dot.micro ~/.config/micro/colorschemes/dot.micro
 symlink ~/dot/config/lsd/lsd.yml ~/.config/lsd/config.yaml
 symlink ~/dot/config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+symlink ~/dot/config/starship/starship.toml ~/.config/starship.toml
 symlink ~/dot/config/zed/settings.json ~/.config/zed/settings.json
