a00183983bdf53ee0de4de1a52eaa6a3e664d689
Baris Inandioglu <68742481+baris-inandi@users.noreply.github.com>
2026-07-08 15:45:23 -0400

    restore symlink mac script

--- setup/void/symlink.sh
+++ setup/void/symlink.sh
@@ -11,6 +11,3 @@ symlink() {
 symlink ~/dot/config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 symlink ~/dot/config/xorg/.xinitrc ~/.xinitrc
 symlink ~/dot/config/xorg/.Xresources ~/.Xresources
-symlink ~/dot/config/firefox/personal/chrome/userChrome.css ~/.mozilla/firefox/personal/chrome/userChrome.css
-symlink ~/dot/config/firefox/personal/chrome/userContent.css ~/.mozilla/firefox/personal/chrome/userContent.css
-symlink ~/dot/config/firefox/personal/user.js ~/.mozilla/firefox/personal/user.js
